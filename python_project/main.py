9d7d6fba62c1e21cbbb3d05b0a868d9ec6740b3e
Елизавета <123663994+ElizawetSilko@users.noreply.github.com>
2023-05-02 17:22:46 +0500

    Add files via upload

--- python_project/main.py
+++ python_project/main.py
@@ -3,7 +3,7 @@ import requests
 #Создать покемона
 response = requests.post('https://pokemonbattle.me:9104/pokemons' , headers={'trainer_token':'4b73c485e194de4704b4c76474ae9052'} , json={
     "name":"Spider",
-    "photo": "https://www.pngmart.com/files/12/Miles-Morales-Spider-Man-PNG-Pic.png",
+    "photo": "https://dolnikov.ru/pokemons/albums/008.png",
     "stage": 2,
     "attack": 1})
 
@@ -11,7 +11,7 @@ print(response.text)
 
 #Поменять имя
 response = requests.put('https://pokemonbattle.me:9104/pokemons' , headers={'trainer_token':'4b73c485e194de4704b4c76474ae9052'} , json={
-    "pokemon_id": 3528,
+    "pokemon_id": 9721,
     "name": "New Name",
     "photo": "https://dolnikov.ru/pokemons/albums/008.png"})
 
@@ -20,7 +20,7 @@ print(response.text)
 
 #Поймать покемона
 response = requests.post('https://pokemonbattle.me:9104/trainers/add_pokeball' , headers={'trainer_token':'4b73c485e194de4704b4c76474ae9052'} , json={
-    "pokemon_id": 3528,
+    "pokemon_id": 9721,
 })
 
 print(response.text)
